4ead0400217dc43e21b1cf075b8bd3a9578901cf
jihun-24k <gi020477@gmail.com>
2022-01-08 00:50:13 +0900

    DI 기본 개념 실습

--- ioc/src/com/company/Encoder.java
+++ ioc/src/com/company/Encoder.java
@@ -5,9 +5,8 @@ import java.util.Base64;
 public class Encoder {
     private IEncoder iEncoder;
 
-    public Encoder(){
-        //this.iEncoder = new Base64Encoder();
-        this.iEncoder = new UrlEncoder();
+    public Encoder(IEncoder iEncoder){
+        this.iEncoder = iEncoder;
     }
 
     public String encode(String message){
--- ioc/src/com/company/Main.java
+++ ioc/src/com/company/Main.java
@@ -16,9 +16,14 @@ public class Main {
         System.out.println(urlResult);
 
         // 추상화를 마친 코드
-        Encoder encoder = new Encoder();
-        String eResult = encoder.encode(url);
-        System.out.println(eResult);
+//        Encoder encoder = new Encoder();
+//        String eResult = encoder.encode(url);
+//        System.out.println(eResult);
+
+        // DI(객체 주입)
+        Encoder encoder = new Encoder(new Base64Encoder());
+        String dResult = encoder.encode(url);
+        System.out.println(dResult);
 
     }
 }
