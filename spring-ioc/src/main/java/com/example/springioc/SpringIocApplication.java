5fe0b5956b38a583f5be0f29ff52995762d558e2
jihun-24k <gi020477@gmail.com>
2022-01-09 18:02:00 +0900

    IoC 실습

--- spring-ioc/src/main/java/com/example/springioc/SpringIocApplication.java
+++ spring-ioc/src/main/java/com/example/springioc/SpringIocApplication.java
@@ -2,12 +2,22 @@ package com.example.springioc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIocApplication.class, args);
-	}
 
+		SpringApplication.run(SpringIocApplication.class, args);
+		ApplicationContext context = ApplicationContextProvider.getContext();
+
+		Base64Encoder base64Encoder = context.getBean(Base64Encoder.class);
+
+		Encoder encoder = new Encoder(base64Encoder);
+		String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
+
+		String result = encoder.encode(url);
+		System.out.println(result);
+	}
 }
