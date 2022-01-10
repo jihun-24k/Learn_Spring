9c0fdd76abc381e4711f23ff0eb2061b6fb124cc
jihun-24k <gi020477@gmail.com>
2022-01-10 16:27:50 +0900

    Qualifier 실습

--- spring-ioc/src/main/java/com/example/springioc/Encoder.java
+++ spring-ioc/src/main/java/com/example/springioc/Encoder.java
@@ -1,15 +1,21 @@
 package com.example.springioc;
 
-import java.util.Base64;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Encoder {
     private IEncoder iEncoder;
 
-    public Encoder(IEncoder iEncoder){
+    public Encoder(@Qualifier("base64Encoder") IEncoder iEncoder){
+        this.iEncoder = iEncoder;
+    }
+
+    public void setIEncoder(IEncoder iEncoder){
         this.iEncoder = iEncoder;
     }
 
     public String encode(String message){
-        return Base64.getEncoder().encodeToString(message.getBytes());
+        return iEncoder.encode(message);
     }
 }
--- spring-ioc/src/main/java/com/example/springioc/SpringIocApplication.java
+++ spring-ioc/src/main/java/com/example/springioc/SpringIocApplication.java
@@ -12,11 +12,11 @@ public class SpringIocApplication {
 		SpringApplication.run(SpringIocApplication.class, args);
 		ApplicationContext context = ApplicationContextProvider.getContext();
 
-		Base64Encoder base64Encoder = context.getBean(Base64Encoder.class);
+		//Base64Encoder base64Encoder = context.getBean(Base64Encoder.class);
+		//UrlEncoder urlEncoder = context.getBean(UrlEncoder.class);
 
-		Encoder encoder = new Encoder(base64Encoder);
+		Encoder encoder = context.getBean(Encoder.class);
 		String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
-
 		String result = encoder.encode(url);
 		System.out.println(result);
 	}
