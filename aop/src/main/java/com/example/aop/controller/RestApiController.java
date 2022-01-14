4ccc790944e0f0fe2d81623f74753385dd261cf8
jihun-24k <gi020477@gmail.com>
2022-01-14 17:03:44 +0900

    AOP 실습 1

--- aop/src/main/java/com/example/aop/controller/RestApiController.java
+++ aop/src/main/java/com/example/aop/controller/RestApiController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.*;
 public class RestApiController {
 
     @GetMapping("/get/{id}")
-    public void get(@PathVariable Long id, @RequestParam String name){
-        System.out.println("get method");
-        System.out.println("get method: " + id);
-        System.out.println("get method: "+ name);
+    public String get(@PathVariable Long id, @RequestParam String name){
+        return id +" " +name;
     }
 
     @PostMapping("/post")
-    public void post(@RequestBody User user){
-        System.out.println("post method: " + user);
+    public User post(@RequestBody User user){
+        return user;
     }
 }
