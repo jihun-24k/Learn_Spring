0262c209efd04169352b25ed56eddd124e859614
jihun-24k <gi020477@gmail.com>
2022-01-16 00:01:46 +0900

    AOP 실습: Decode, Encode

--- aop/src/main/java/com/example/aop/controller/RestApiController.java
+++ aop/src/main/java/com/example/aop/controller/RestApiController.java
@@ -1,6 +1,7 @@
 package com.example.aop.controller;
 
 
+import com.example.aop.annotation.Decode;
 import com.example.aop.annotation.Timer;
 import com.example.aop.dto.User;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +25,13 @@ public class RestApiController {
     public void delete() throws InterruptedException {
         //db logic
         Thread.sleep(1000*2);
+    }
+
+    @Decode
+    @PutMapping("/put")
+    public User put(@RequestBody User user){
+        System.out.println("put");
+        System.out.println(user);
+        return user;
     }
 }
