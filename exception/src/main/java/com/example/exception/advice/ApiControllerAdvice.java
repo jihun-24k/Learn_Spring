4e52dbfc653c23ce7d1f8dd1d36ae87a1bce86dd
jihun-24k <gi020477@gmail.com>
2022-01-24 21:30:02 +0900

    실무에서 사용되는 예외처리들

---
+++ exception/src/main/java/com/example/exception/advice/ApiControllerAdvice.java
@@ -0,0 +1,76 @@
+package com.example.exception.advice;
+
+import com.example.exception.controller.ApiController;
+import com.example.exception.dto.Error;
+import com.example.exception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice(basePackageClasses = ApiController.class)
+public class ApiControllerAdvice {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity exception(Exception e){
+        System.out.println(e.getClass().getName());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest httpServletRequest){
+        List<Error> errorList = new ArrayList<>();
+
+        BindingResult bindingResult = e.getBindingResult();
+        bindingResult.getAllErrors().forEach(error -> {
+            FieldError field = (FieldError) error;
+
+            String fieldName = field.getField();
+            String message = field.getDefaultMessage();
+            String value = field.getRejectedValue().toString();
+
+            System.out.println("-----------------");
+            System.out.println(fieldName);
+            System.out.println(message);
+            System.out.println(value);
+
+            Error error1 = new Error();
+            error1.setField(fieldName);
+            error1.setMessage(message);
+            error1.setInvalidValue(value);
+
+            errorList.add(error1);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorList(errorList);
+        errorResponse.setMessage("");
+        errorResponse.setRequestUrl(httpServletRequest.getRequestURI());
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.toString());
+        errorResponse.setResultCode("FAIL");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    public ResponseEntity missingServletRequestParameterException(MissingServletRequestParameterException e){
+        String fieldName = e.getParameterName();
+        String fieldType = e.getParameterType();
+        String invalidValue = e.getMessage();
+
+        System.out.println(fieldName);
+        System.out.println(fieldType);
+        System.out.println(invalidValue);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
--- exception/src/main/java/com/example/exception/controller/ApiController.java
+++ exception/src/main/java/com/example/exception/controller/ApiController.java
@@ -3,22 +3,32 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
 
-        int a = 10 + age;
         return user;
     }
 
@@ -26,11 +36,5 @@ public class ApiController {
     public User post(@Valid @RequestBody User user){
         System.out.println(user);
         return user;
-    }
-
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
-        System.out.println("controller error");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
