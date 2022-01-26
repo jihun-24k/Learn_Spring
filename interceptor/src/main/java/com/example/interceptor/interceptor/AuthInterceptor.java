5207d7495a9f2ec8c956afead25b13c3b32aa992
jihun-24k <gi020477@gmail.com>
2022-01-26 16:23:44 +0900

    Public Interceptor 실습

--- interceptor/src/main/java/com/example/interceptor/interceptor/AuthInterceptor.java
+++ interceptor/src/main/java/com/example/interceptor/interceptor/AuthInterceptor.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 @Slf4j
 @Component
@@ -17,9 +19,27 @@ public class AuthInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String url  =request.getRequestURI();
+
+        URI uri = UriComponentsBuilder.fromUriString(request.getRequestURI())
+                .query(request.getQueryString())
+                .build()
+                .toUri();
+
         log.info("request url : {}", url);
         boolean hasAnnotation = checkAnnotation(handler, Auth.class);
         log.info("has annotation : {}", hasAnnotation);
+
+        // 나의 서버는 모두 public으로 동작을 하는데
+        // 단 Auth 권한을 가진 요청에 대해서는 세션, 쿠키,
+        if (hasAnnotation){
+            // 권한체크
+            String query = uri.getQuery();
+            log.info("query : {}", query);
+            if (query.equals("name = jihun")){
+                return true;
+            }
+            return false;
+        }
 
         return true;
     }
