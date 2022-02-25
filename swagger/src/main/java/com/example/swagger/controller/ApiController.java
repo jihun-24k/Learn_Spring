1b7b00a601692af263932105ddb42a2619cb4902
jihun-24k <gi020477@gmail.com>
2022-02-26 00:48:39 +0900

    Swagger: API를 문서화

--- swagger/src/main/java/com/example/swagger/controller/ApiController.java
+++ swagger/src/main/java/com/example/swagger/controller/ApiController.java
@@ -1,9 +1,11 @@
 package com.example.swagger.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.swagger.dto.UserReq;
+import com.example.swagger.dto.UserRes;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
 
+@Api(tags = {"API 정보를 제공하는 Controller"})
 @RestController
 @RequestMapping("/api")
 public class ApiController {
@@ -11,5 +13,30 @@ public class ApiController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(name = "x", value = "x값"),
+                    @ApiImplicitParam(name = "y", value = "y값")
+            }
+    )
+    @GetMapping("/plus/{x}")
+    public int plus(
+            @PathVariable int x,
+            @RequestParam int y){
+        return x+y;
+    }
+
+    @ApiResponse(code = 502, message = "사용자의 나이가 10살 이하일때")
+    @ApiOperation(value = "사용자의 이름과 나이를 리턴하는 메소드")
+    @GetMapping("/user")
+    public UserRes user(UserReq userReq){
+        return new UserRes(userReq.getName(), userReq.getAge());
+    }
+
+    @PostMapping("/user")
+    public UserRes userPost(@RequestBody UserReq userReq){
+        return new UserRes(userReq.getName(), userReq.getAge());
     }
 }
